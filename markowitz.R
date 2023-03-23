75480deb9c453c28b8f1ae520e744edf8c2c2a0b
Julian <julian.righ.sampedro@gmail.com>
2023-03-23 10:45:06 +0100

    Add files via upload

--- markowitz.R
+++ markowitz.R
@@ -3,6 +3,10 @@
 #------------------------------------------------
 
 library(quantmod)
+library(tidyverse)
+library(PerformanceAnalytics)
+library(corrplot)
+
 # retrieve stock prices from Yahoo finance
 # Tesla, Inc.
 TSLA <- getSymbols("TSLA", src = "yahoo", from = "2020-01-01", to = "2022-06-01", auto.assign = FALSE)
@@ -41,7 +45,6 @@ covs = round(cov(assets))
 # APPLE  3146  1044 1015  568
 # META   3088  1015 3382 1166
 # AMZN   1741   568 1166  628
-library(corrplot)
 Assets <- cor(assets)
 #            TESLA      APPLE       META       AMZN
 # TESLA  1.0000000  0.9806590 -0.9958627 -0.6402810
@@ -52,74 +55,209 @@ corrplot(Assets, method="number")
 
 
 
-#------------------------------------------------
-# CHOLESKI TO SIMULATE FROM MULTIVARIATE GAUSSIAN
-#------------------------------------------------
+# retrieve stock prices from Yahoo finance
+# Tesla, Inc.
+TSLA <- getSymbols("TSLA", src = "yahoo", from = "2020-01-01", to = "2022-06-01", 
+                   auto.assign = FALSE)
+# Apple Inc.
+AAPL <- getSymbols("AAPL", src = "yahoo", from = "2020-01-01", to = "2022-06-01",
+                   auto.assign = FALSE)
+# Meta Platforms, Inc.
+META <- getSymbols("META", src = "yahoo", from = "2020-01-01", to = "2022-06-01", 
+                   auto.assign = FALSE)
+# Amazon.com, Inc.
+AMZN <- getSymbols("AMZN", src = "yahoo", from = "2020-01-01", to = "2022-06-01",
+                   auto.assign = FALSE)
+# time series plot of the different stocks
+plot(TSLA$TSLA.Adjusted, main = 'Stock prices ...')
+lines(AAPL$AAPL.Adjusted, col = 'darkred')
+lines(META$META.Adjusted, col = 'darkblue')
+lines(AMZN$AMZN.Adjusted, col = 'darkgreen')
 
-# parameters
-n <- 10000
-assets <- matrix(cbind(TSLA$TSLA.Adjusted, AAPL$AAPL.Adjusted, META$META.Adjusted,
-                       AMZN$AMZN.Adjusted), byrow = FALSE, ncol = 4)
-colnames(assets) <- c("TESLA", "APPLE", "META", "AMZN")
-Assets <- cor(assets)
-# correlations
-rho_12 <- Assets[1,2]; rho_13 <- Assets[1,3]; rho_14 <- Assets[1,4] 
-rho_23 <- Assets[2,3]; rho_24 <- Assets[2,4]; rho_34 <- Assets[3,4]
-# standard deviations
-sds = apply(assets,2, sd)
-s1 <- sds[[1]]; s2 <- sds[[2]]; s3 <- sds[[3]]; s4 <- sds[[4]]
-# means
-mu <- as.numeric(colMeans(assets))
-# covariance matrix, eg. cov_12 = s1*s2*rho_12
-Sigma <- round(matrix(c(s1^2, s1*s2*rho_12, s1*s3*rho_13, s1*s4*rho_14,
-                  s1*s2*rho_12, s2^2, s2*s3*rho_23, s2*s4*rho_24, 
-                  s1*s3*rho_13, s2*s3*rho_23, s3^2, s2*s4*rho_24,
-                  s1*s4*rho_14, s2*s4*rho_24,s3*s4*rho_34,s4^2), nrow = 4))
+# daily log returns
+TSLAreturns = Return.calculate(TSLA[,6],method="log")
+TSLAreturns = TSLAreturns[(-1)]
+AAPLreturns = Return.calculate(AAPL[,6],method="log")
+AAPLreturns = AAPLreturns[(-1)]
+METAreturns = Return.calculate(META[,6],method="log")
+METAreturns = METAreturns[(-1)]
+AMZNreturns = Return.calculate(AMZN[,6],method="log")
+AMZNreturns = AMZNreturns[(-1)]
 
-# function to sample from a Multivariate Normal distribution
-mv.cholesky <- function(n, mu, Sigma) {
-  
-  d <- length(mu)
-  Q <- chol(Sigma)
-  Z <- matrix(rnorm(n*d), nrow = n, ncol = d)
-  X <- Z %*% Q +  rep(1,n) %*% t(mu)
-  X <- data.frame(X)
-  return(X)
+
+returns<- data.frame(cbind(TSLAreturns, AAPLreturns, METAreturns, AMZNreturns))
+
+
+# mean returns
+mean.returns <- as.numeric(colMeans(returns))
+# anualized risk (stadard deviation) of returns
+cov.returns.anualized <- cov(returns) * 252
+# simulations
+nsim <- 10000
+# storage objects
+Aweights <- matrix(rep(0, nsim*4), nrow = nsim, ncol = 4)
+Returns.Port <- numeric(nsim)
+Risk.Port <- numeric(nsim)
+weights <- numeric(4)
+Weights <-matrix(rep(0, nsim*4), nrow = nsim, ncol = 4)
+
+set.seed(2023)
+for(i in 1:nsim) {
+  weights <- runif(4)
+  sweights <- sum(weights)
+  Weights[i, ] <- weights/sweights
+  # Portfolio return
+  returns.Port <- sum((weights/sweights) * mean.returns)
+  Returns.Port[i] <- ((returns.Port + 1)^252) - 1
+  # Rortfolio risk
+  Risk.Port[i] <- sqrt(t((weights/sweights)) %*% (cov.returns.anualized  
+                                                  %*% (weights/sweights)))
 }
+Portfolios <- matrix(cbind(Weights,Returns.Port,Risk.Port), 
+                     byrow = FALSE, ncol = 6)
+colnames(Portfolios) <- c("TESLA", "APPLE", "META", "AMZN", "Return", "Risk")
+head(round(Portfolios,4))
 
-# call the function and create a sample of desired size
-set.seed(1986)
-random.sample <- mv.cholesky(n = n, mu = mu, Sigma = Sigma)
+# portfolios with risk < 36% and return > 25%
+Portfolios <- data.frame(Portfolios)
+
+Portfolios %>%
+  ggplot(aes(x = Risk, y = Return, color = Risk)) +
+  geom_point() +
+  scale_y_continuous(labels = scales::percent) +
+  scale_x_continuous(labels = scales::percent) +
+  scale_color_gradient(low = "firebrick4", high = "coral") +
+  labs(x = 'Annualized Risk (standard deviation)',
+       y = 'Annualized Returns',
+       title = "Portfolios with risk < 30% and return > 20%") +
+  geom_point(aes(x = Risk,
+                 y = Return), data = Portfolios[which(Portfolios$Risk < 0.36 & 
+                                                        Portfolios$Return > 0.25), ], 
+             color = 'black', size = 5) +
+  annotate('text', x = 0.39, y = 0.7, label = "Portfolio selection (black)") +
+  theme_minimal()
+
+PortfolioSelection <- Portfolios[which(Portfolios$Risk < 0.36 &
+                                         Portfolios$Return > 0.25), ]
+
+barplot(colMeans(PortfolioSelection[, 1:4]), 
+        main = 'Optimal composition for Portfolios with risk < 36% and return > 25%',
+        xlab = 'Assets', ylab = 'Weight in Portfolio',
+        col = c("blue", "darkblue", "darkred", "red"),
+        legend = round(colMeans(PortfolioSelection[, 1:4]), 4)*100, beside = TRUE)
+
+#----
+# end
+#----
+
+#------------------
+# tangent portfolio
+#------------------
+
+# retrieve stock prices from Yahoo finance
+# Tesla, Inc.
+TSLA <- getSymbols("TSLA", src = "yahoo", from = "2020-01-01", to = "2022-06-01", 
+                   auto.assign = FALSE)
+# Apple Inc.
+AAPL <- getSymbols("AAPL", src = "yahoo", from = "2020-01-01", to = "2022-06-01",
+                   auto.assign = FALSE)
+# Meta Platforms, Inc.
+META <- getSymbols("META", src = "yahoo", from = "2020-01-01", to = "2022-06-01", 
+                   auto.assign = FALSE)
+# Amazon.com, Inc.
+AMZN <- getSymbols("AMZN", src = "yahoo", from = "2020-01-01", to = "2022-06-01",
+                   auto.assign = FALSE)
+# time series plot of the different stocks
+plot(TSLA$TSLA.Adjusted, main = 'Stock prices ...')
+lines(AAPL$AAPL.Adjusted, col = 'darkred')
+lines(META$META.Adjusted, col = 'darkblue')
+lines(AMZN$AMZN.Adjusted, col = 'darkgreen')
+
+# daily log returns
+TSLAreturns = Return.calculate(TSLA[,6],method="log")
+TSLAreturns = TSLAreturns[(-1)]
+AAPLreturns = Return.calculate(AAPL[,6],method="log")
+AAPLreturns = AAPLreturns[(-1)]
+METAreturns = Return.calculate(META[,6],method="log")
+METAreturns = METAreturns[(-1)]
+AMZNreturns = Return.calculate(AMZN[,6],method="log")
+AMZNreturns = AMZNreturns[(-1)]
+
+returns<- data.frame(cbind(TSLAreturns, AAPLreturns, METAreturns, AMZNreturns))
 
 
-## plotting
+# mean returns
+mean.returns <- as.numeric(colMeans(returns))
+# anualized risk (stadard deviation) of returns
+cov.returns.anualized <- cov(returns) * 252
+# simulations
+nsim <- 10000
+# storage objects
+Aweights <- matrix(rep(0, nsim*4), nrow = nsim, ncol = 4)
+Returns.Port <- numeric(nsim)
+Risk.Port <- numeric(nsim)
+weights <- numeric(4)
+Weights <-matrix(rep(0, nsim*4), nrow = nsim, ncol = 4)
 
-library(ggplot2)
-library(gridExtra)
-library(grid)
+set.seed(2023)
+for(i in 1:nsim) {
+  weights <- runif(4)
+  sweights <- sum(weights)
+  Weights[i, ] <- weights/sweights
+  # Portfolio return
+  returns.Port <- sum((weights/sweights) * mean.returns)
+  Returns.Port[i] <- ((returns.Port + 1)^252) - 1
+  # Rortfolio risk
+  Risk.Port[i] <- sqrt(t((weights/sweights)) %*% (cov.returns.anualized  
+                                                  %*% (weights/sweights)))
+}
+Portfolios <- matrix(cbind(Weights,Returns.Port,Risk.Port), 
+                     byrow = FALSE, ncol = 6)
+colnames(Portfolios) <- c("TESLA", "APPLE", "META", "AMZN", "Return", "Risk")
+head(round(Portfolios,4))
 
-htop <- ggplot(data=random.sample, aes(x=X1)) + 
-  geom_histogram(aes(y=..density..), fill = "grey90", color = "black", binwidth = 0.3) + 
-  stat_density(colour = "red3", geom="line", size = 1.2, position="identity", show.legend=FALSE) +
-  theme(axis.title.x = element_blank(),
-        panel.background = element_blank())
+# tangent portfolio (maximizing mean return over risk)
+Portfolios <- data.frame(Portfolios)
 
-blank <- ggplot() + geom_point(aes(1,1), colour="white") +
-  theme(axis.ticks=element_blank(), panel.background=element_blank(), panel.grid=element_blank(),
-        axis.text.x=element_blank(), axis.text.y=element_blank(), axis.title.x=element_blank(), axis.title.y=element_blank())
+p1<-  ggplot(Portfolios, aes(x = Risk, y = Return, color = Risk)) +
+  geom_point() +
+  scale_y_continuous(labels = scales::percent) +
+  scale_x_continuous(labels = scales::percent) +
+  scale_colour_gradient2() +
+  geom_point(aes(x = Risk,
+                 y = Return), data = Portfolios[which.max(mean(Portfolios$Return)/Portfolios$Risk), ], 
+             color = 'black', size = 3) +
+  annotate('text', x = 0.39, y = 0.7, label = "Portfolio selection (black)") +
+  labs(title = 'Tangent Portfolio - Maximizing the Sharpe ratio',
+       subtitle = 'Portfolio of 4 stocks retrieved from Yahoo finance',
+       y="Annualized Returns", x="Annualized Risk (standard deviation)") +
+  theme(axis.text=element_text(size=8),
+        axis.title=element_text(size=8),
+        plot.subtitle=element_text(size=10, face="italic", color="darkred"),
+        panel.background = element_rect(fill = "white", colour = "grey50"),
+        panel.grid.major = element_line(colour = "grey90"))
 
-scatter <- ggplot(data=random.sample, aes(x=X1, y=X2)) + 
-  geom_point(size = 0.6, pch=3) + 
-  stat_ellipse(level = 0.80, linetype = 1, color="red2", size=1) +
-  stat_ellipse(level = 0.95, linetype = 1, color="red2", size=0.8) +
-  stat_ellipse(level = 0.99, color="red3", size=0.6) +
-  theme_light()
+# Portfolio selection
+PortfolioSelection <- Portfolios[which(Portfolios$Risk < 0.36 &
+                                         Portfolios$Return > 0.25), ]
 
-hright <- ggplot(data=random.sample, aes(x=X2)) + 
-  geom_histogram(aes(y=..density..), fill = "grey90", color = "black", binwidth = 0.3) + 
-  stat_density(colour = "red3", geom="line", size = 1.2, position="identity", show.legend=FALSE) +
-  coord_flip() + theme(axis.title.y = element_blank(),
-                       panel.background = element_blank())
+dataset <- data.frame('Assets' = colnames(PortfolioSelection[1:4]), 
+                      'Weights' = colMeans(PortfolioSelection[, 1:4]))
+rownames(dataset) <- NULL
 
-grid.arrange(htop, blank, scatter, hright, ncol=2, nrow=2, widths=c(4, 1), heights=c(1, 4),
-             top=textGrob("Scatterplot of the simulated joint distribution of asset 1 (TESLA) and asset 2 (APPLE)"))
+ggplot(dataset, aes(x = Assets, y = Weights, fill = Weights)) +
+  geom_bar(stat="identity") + 
+  scale_colour_gradient2() +
+  labs(title = 'Portfolio selection for risk < 0.36 and return > 0.25',
+       subtitle = 'Portfolio of 4 stocks retrieved from Yahoo financet',
+       y="Weight in the portfolio", x="Assets") +
+  theme(axis.text=element_text(size=8),
+        axis.title=element_text(size=8),
+        plot.subtitle=element_text(size=10, face="italic", color="darkred"),
+        panel.background = element_rect(fill = "white", colour = "grey50"),
+        panel.grid.major = element_line(colour = "grey90"))
+
+
+#----
+# end
+#----
